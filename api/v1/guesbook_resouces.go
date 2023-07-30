f9f8d926dff5bdd8cbc1b2b120fec672bab6fe15
Salih Bozkaya <salihbozkaya@Salihs-MacBook-Pro.local>
2023-07-30 20:41:14 +0300

    add container spec

--- api/v1/guesbook_resouces.go
+++ api/v1/guesbook_resouces.go
@@ -22,7 +22,10 @@ func (g *Guestbook) Construct() []metav1.Object {
 			},
 		},
 		Spec: corev1.ServiceSpec{
-			Selector: map[string]string{},
+			Selector: map[string]string{
+				"app.kubernetes.io/name":      "guestbook",
+				"app.kubernetes.io/component": "frontend",
+			},
 			Ports: []corev1.ServicePort{
 				{
 					Protocol: corev1.ProtocolTCP,
@@ -76,7 +79,7 @@ func (g *Guestbook) Construct() []metav1.Object {
 							},
 							Env: []corev1.EnvVar{
 								{
-									Name:  "GET_ENV_FROM",
+									Name:  "GET_HOST_FROM",
 									Value: "dns",
 								},
 							},
@@ -88,6 +91,86 @@ func (g *Guestbook) Construct() []metav1.Object {
 	}
 
 	obj = append(obj, frontendDeployment)
+
+	backendService := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-backend", g.Name),
+			Namespace: g.Namespace,
+			Labels: map[string]string{
+				"app.kubernetes.io/name":      "mongo",
+				"app.kubernetes.io/component": "backend",
+			},
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				"app.kubernetes.io/name":      "mongo",
+				"app.kubernetes.io/component": "backend",
+			},
+			Ports: []corev1.ServicePort{
+				{
+					Protocol: corev1.ProtocolTCP,
+					Port:     27010,
+				},
+			},
+		},
+	}
+
+	obj = append(obj, backendService)
+
+	backendDeployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-backend", g.Name),
+			Namespace: g.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &g.Spec.BackendReplicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app.kubernetes.io/name":      "mongo",
+					"app.kubernetes.io/component": "backend",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app.kubernetes.io/name":      "mongo",
+						"app.kubernetes.io/component": "backend",
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  g.Name,
+							Image: g.Spec.BackendImage,
+							Ports: []corev1.ContainerPort{
+								{
+									ContainerPort: 27010,
+								},
+							},
+							Resources: corev1.ResourceRequirements{
+								Requests: corev1.ResourceList{
+									corev1.ResourceCPU:    *resource.NewMilliQuantity(int64(100), resource.DecimalSI),
+									corev1.ResourceMemory: *resource.NewQuantity(int64(100*1024*1024), resource.BinarySI),
+								},
+							},
+							Env: []corev1.EnvVar{
+								{
+									Name:  "GET_HOST_FROM",
+									Value: "dns",
+								},
+							},
+							Args: []string{
+								"--bind_ip",
+								"0.0.0.0",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	obj = append(obj, backendDeployment)
 
 	return obj
 }
